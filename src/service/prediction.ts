e525e1dd64aac54ea037feab073067a26b05a9d3
aktharazif <hello@aktharazif.com>
2026-04-10 13:57:02 +0530

    model ordering

--- src/service/prediction.ts
+++ src/service/prediction.ts
@@ -1,5 +1,6 @@
 import db, { schema } from "../drizzle";
+import { desc } from "drizzle-orm";
 
 export async function getModels() {
-  return db.select().from(schema.model);
+  return db.select().from(schema.model).orderBy(desc(schema.model.score));
 }
